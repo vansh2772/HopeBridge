20ba56638d2072a1f36c1071605b7f1193fc7639
vansh2772 <vansh6dec@gmail.com>
2025-04-05 12:34:22 +0530

    Delete config.js

--- config.js
+++
@@ -1,4 +0,0 @@
-const CONFIG = {
-    API_KEY: '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', // Replace with your actual API key
-    API_URL: 'https://api-inference.huggingface.co/models/mistralai/Mistral-7B-Instruct-v0.2'
-};
