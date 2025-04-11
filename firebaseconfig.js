ee31fbc45329b18e09b494ad8c24bdd19ce5feb7
vansh2772 <vansh6dec@gmail.com>
2025-04-11 22:03:34 +0530

    Create firebaseconfig.js

---
+++ firebaseconfig.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC0f1f_taXBCvo3FCgqE2UzG0MtMYYCzHU",
+  authDomain: "hopebridge-d1807.firebaseapp.com",
+  databaseURL: "https://hopebridge-d1807-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "hopebridge-d1807",
+  storageBucket: "hopebridge-d1807.firebasestorage.app",
+  messagingSenderId: "145634761597",
+  appId: "1:145634761597:web:7f4af1bdadb5969cb6ca14",
+  measurementId: "G-F0SHQQ6558"
+};
